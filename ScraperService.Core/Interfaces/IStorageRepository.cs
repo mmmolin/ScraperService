95d859cf974ac9e44e61c178526b3000d676c3fb
Martin <mmolin@telia.com>
2020-12-31 01:42:15 +0100

    Adds DataStorageRepository

---
+++ ScraperService.Core/Interfaces/IStorageRepository.cs
@@ -0,0 +1,15 @@
+﻿using ScraperService.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperService.Core.Interfaces
+{
+    public interface IStorageRepository
+    {
+        public void AddData(List<ScrapeData> entities);
+        public List<ScrapeData> GetDailyData();
+    }
+}
---
+++ ScraperService.Infrastructure/Data/DataStorageRepository.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using ScraperService.Core.Entities;
+using ScraperService.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperService.Infrastructure.Data
+{
+    public class DataStorageRepository : IStorageRepository
+    {
+        private IMongoClient client;
+        public DataStorageRepository(IMongoClient client)
+        {
+            this.client = client;
+        }
+        public void AddData(List<ScrapeData> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ScrapeData> GetDailyData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
